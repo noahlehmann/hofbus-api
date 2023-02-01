41880ca5096ec519237ac47c438e335b55afcf19
Noah Lehmann <noah.lehmann@protonmail.com>
2023-02-01 13:00:06 +0100

    feature/22-document-swagger-endpoints-for-gtfs-endpoints: document agency

--- api/src/main/java/de/noahlehmann/hofbusapi/controller/GtfsController.java
+++ api/src/main/java/de/noahlehmann/hofbusapi/controller/GtfsController.java
@@ -1,13 +1,11 @@
 package de.noahlehmann.hofbusapi.controller;
 
-import de.noahlehmann.hofbusapi.util.gtfs.model.Agency;
-import de.noahlehmann.hofbusapi.util.gtfs.model.Calendar;
-import de.noahlehmann.hofbusapi.util.gtfs.model.CalendarDate;
-import de.noahlehmann.hofbusapi.util.gtfs.model.FeedInfo;
-import de.noahlehmann.hofbusapi.util.gtfs.model.Route;
-import de.noahlehmann.hofbusapi.util.gtfs.model.Stop;
-import de.noahlehmann.hofbusapi.util.gtfs.model.StopTime;
-import de.noahlehmann.hofbusapi.util.gtfs.model.Trip;
+import de.noahlehmann.hofbusapi.util.gtfs.model.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,22 +19,49 @@ import java.util.List;
 @RequestMapping("gtfs")
 public class GtfsController {
 
-    @GetMapping("/agency")
+    @Operation(summary = "Transit agencies with service represented in this dataset.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved list of agencies",
+                            content = @Content(schema = @Schema(implementation = Agency.class))),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/agency", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<Agency>> getAgencies() {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/stops")
+    @Operation(summary = "Stops where vehicles pick up or drop off riders. Also defines stations and station entrances.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/stops", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<Stop>> getStops() {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/routes")
+    @Operation(summary = "Transit routes. A route is a group of trips that are displayed to riders as a single service.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/routes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<Route>> getRoutes() {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/trips")
+    @Operation(summary = "Trips for each route. A trip is a sequence of two or more stops that occur during a specific time period.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/trips", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<Trip>> getTrips(
             @RequestParam(name = "route_id", required = false) String routeId,
             @RequestParam(name = "service_id", required = false) String serviceId
@@ -44,19 +69,37 @@ public class GtfsController {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/calendar")
+    @Operation(summary = "Service dates specified using a weekly schedule with start and end dates. This file is required unless all dates of service are defined in calendar_dates.txt.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/calendar", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<Calendar>> getCalendar() {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/calendar_dates")
+    @Operation(summary = "Exceptions for the services defined in the calendar.txt. If calendar.txt is omitted, then calendar_dates.txt is required and must contain all dates of service.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/calendar_dates", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<CalendarDate>> getCalendarDates(
             @RequestParam(name = "date", required = false) Date date
     ) {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/stop_times")
+    @Operation(summary = "Times that a vehicle arrives at and departs from stops for each trip.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/stop_times", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<StopTime>> getStopTimes(
             @RequestParam(name = "trip_id", required = false) String tripId,
             @RequestParam(name = "stop_id", required = false) String stopId
@@ -64,7 +107,13 @@ public class GtfsController {
         return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/feed_info")
+    @Operation(summary = "Dataset metadata, including publisher, version, and expiration information.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of agencies"),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+                    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+            })
+    @GetMapping(value = "/feed_info", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<List<FeedInfo>> getFeedInfo() {
         return ResponseEntity.ok(null);
     }
--- api/src/main/java/de/noahlehmann/hofbusapi/util/gtfs/model/Agency.java
+++ api/src/main/java/de/noahlehmann/hofbusapi/util/gtfs/model/Agency.java
@@ -1,5 +1,6 @@
 package de.noahlehmann.hofbusapi.util.gtfs.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(name = "Agency", description = "Transit agencies with service represented in this dataset.")
 public class Agency {
 
 
@@ -25,28 +27,53 @@ public class Agency {
      * - Required when the dataset contains data for multiple transit agencies.
      * - Optional otherwise.
      */
+    @Schema(name = "agency_id",
+            description = "Identifies a transit brand which is often synonymous with a transit agency. " +
+                    "Note that in some cases, such as when a single agency operates multiple separate services, " +
+                    "agencies and brands are distinct. A dataset may contain data from multiple agencies.",
+            example = "hofbus"
+    )
     private String agencyId;
 
     /**
      * Full name of the transit agency.
      */
+    @Schema(name = "agency_name",
+            description = "Full name of the transit agency.",
+            example = "HofBus GmbH"
+    )
     private String agencyName;
 
     /**
      * URL of the transit agency.
      */
+    @Schema(name = "agency_url",
+            description = "URL of the transit agency.",
+            example = "www.hofbus.de"
+    )
     private String agencyUrl;
 
     /**
      * Timezone where the transit agency is located.
      * If multiple agencies are specified in the dataset, each must have the same agency_timezone.
      */
+    @Schema(name = "agency_timezone",
+            description = "Timezone where the transit agency is located." +
+                    "If multiple agencies are specified in the dataset, each must have the same agency_timezone.",
+            example = "Europe/Berlin"
+    )
     private String agencyTimezone;
 
     /**
      * Primary language used by this transit agency.
      * Should be provided to help GTFS consumers choose capitalization rules and other language-specific settings for the dataset.
      */
+    @Schema(name = "agency_lang",
+            description = "Primary language used by this transit agency." +
+                    "Should be provided to help GTFS consumers choose capitalization rules and " +
+                    "other language-specific settings for the dataset.",
+            example = "de"
+    )
     private String agencyLang;
 
     /**
@@ -55,17 +82,33 @@ public class Agency {
      * It may contain punctuation marks to group the digits of the number.
      * Dialable text (for example, TriMet's "503-238-RIDE") is permitted, but the field must not contain any other descriptive text.
      */
+    @Schema(name = "agency_phone",
+            description = "A voice telephone number for the specified agency." +
+                    "This field is a string value that presents the telephone number as typical for the agency's service area." +
+                    "It may contain punctuation marks to group the digits of the number." +
+                    "Dialable text (for example, TriMet's \"503-238-RIDE\") is permitted, " +
+                    "but the field must not contain any other descriptive text.",
+            example = "0123 4567890"
+    )
     private String agencyPhone;
 
     /**
      * URL of a web page that allows a rider to purchase tickets or other fare instruments for that agency online.
      */
+    @Schema(name = "agency_fare_url",
+            description = "URL of a web page that allows a rider to purchase tickets or other fare instruments for that agency online.",
+            example = "www.hofbus.de/tickets"
+    )
     private String agencyFareUrl;
 
     /**
      * Email address actively monitored by the agency’s customer service department.
      * This email address should be a direct contact point where transit riders can reach a customer service representative at the agency.
      */
+    @Schema(name = "agency_email",
+            description = "Email address actively monitored by the agency’s customer service department.",
+            example = "info@hofbus.de"
+    )
     private String agencyEmail;
 
 }
